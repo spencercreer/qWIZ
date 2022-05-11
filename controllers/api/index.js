fa795df149f812cc6e964e327247ca837b13d199
spencercreer <spencercreer@gmail.com>
2022-05-11 08:20:32 -0700

    highscore routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 
 const userRoutes = require('./userRoutes')
+const highscoreRoutes = require('./highScoreRoutes')
 
 router.use('/user', userRoutes)
+router.use('/highscore', highscoreRoutes)
 
 module.exports = router
