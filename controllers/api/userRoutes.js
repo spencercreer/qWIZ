cbc7acaa805c836ff26df7c4f6395f3e20c21781
spencercreer <spencercreer@gmail.com>
2022-05-10 17:46:30 -0700

    user create route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,17 +4,11 @@ const { User } = require('../../models')
 router.post('/', async (req, res) => {
     let { email, password } = req.body
     try {
-        const user = await User.findOne({
-            where: {
-                email
-            }
-        })
+        const user = await User.create(req.body)
 
         if (!user) {
-            return res.status(400).json({ message: 'Invalid user email' })
+            return res.status(400).json({ message: 'There was an error creating the user' })
         }
-
-        //TODO: validate password
 
         //TODO: implement session
 
