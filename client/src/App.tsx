185b69847ba75f166504132434ce77b9a7169e24
spencercreer <spencercreer@gmail.com>
2022-09-14 22:09:28 -0700

    client proxy and css scripts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-orange-500"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
