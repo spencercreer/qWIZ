a57543d8bd76130e683b1f056476adb60cbc7756
spencercreer <spencercreer@gmail.com>
2022-09-25 21:53:58 -0700

    header css

--- client/src/pages/QuizPage.tsx
+++ client/src/pages/QuizPage.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function QuizPage() {
-  return (
-    <h1>Quiz</h1>
-  );
+    const [time, setTime] = useState(75)
+    
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(prevTime => prevTime -= 1)
+        }, 1000);
+    
+        return () => clearInterval(timer);
+    }, [])
+    return (
+        <div className="">
+            <h1 className="">Math Quiz</h1>
+            <div className="">
+                <p className=""></p>
+                <div className="">
+                    <input type="text" value="" />
+                    <button className="btn" type="button">Submit</button>
+                </div>
+            </div>
+            <p className="">{time}</p>
+        </div>
+    );
 }
 
 export default QuizPage;
